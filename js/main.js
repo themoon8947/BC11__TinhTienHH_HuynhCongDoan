78aac92f3aae9243bc9a9dd51e0ed840ad37c6b7
themoon8947 <fz5soul@gmail.com>
2021-05-30 01:33:26 +0700

    undate

--- js/main.js
+++ js/main.js
@@ -7,38 +7,39 @@ document.getElementById("btnThanhTien").onclick=function(event){
     var thanhTien=0;
     
     if(soLuong>=100){
-        thanhTien=(donGia*49)+(50*(donGia*((100-8)/100)))+((soLuong-99)*(donGia*((100-12)/100)));
+        thanhTien=(donGia*49)+(51*(donGia*((100-8)/100)))+((soLuong-100)*(donGia*((100-12)/100)));
     }else if(soLuong>=50){
         thanhTien=(donGia*49)+((soLuong-49)*(donGia*((100-8)/100)))
     }
-    else
+    else{
         thanhTien=soLuong*donGia;
-
+        }
     
 
     var chuyenDoi = new Intl.NumberFormat('vn-VN');
     var tienChuyenDoi = chuyenDoi.format(thanhTien);
     var dongGiaChuyenDoi=chuyenDoi.format(donGia);
-    //tạo thead
-
-    var taoTR_thead=document.createElement("tr");
     
-    //tạo 3 cột
-    var taoTH_tenHang=document.createElement("th");
-    taoTH_tenHang.innerHTML="Tên hàng";
-    var taoTH_SL=document.createElement("th");
-    taoTH_SL.innerHTML="Số lượng";
-    var taoTH_donGia=document.createElement("th");
-    taoTH_donGia.innerHTML="Đơn Giá";
-    var taoTH_thanhTien=document.createElement("th");
-    taoTH_thanhTien.innerHTML="Thành tiền";
+    document.getElementById("thead").style.opacity="1";
+    //tạo thead
+    // var taoTR_thead=document.createElement("tr");
+    
+    // //tạo 3 cột
+    // var taoTH_tenHang=document.createElement("th");
+    // taoTH_tenHang.innerHTML="Tên hàng";
+    // var taoTH_SL=document.createElement("th");
+    // taoTH_SL.innerHTML="Số lượng";
+    // var taoTH_donGia=document.createElement("th");
+    // taoTH_donGia.innerHTML="Đơn Giá";
+    // var taoTH_thanhTien=document.createElement("th");
+    // taoTH_thanhTien.innerHTML="Thành tiền";
 
-    taoTR_thead.appendChild(taoTH_tenHang);
-    taoTR_thead.appendChild(taoTH_SL);
-    taoTR_thead.appendChild(taoTH_donGia);
-    taoTR_thead.appendChild(taoTH_thanhTien);
+    // taoTR_thead.appendChild(taoTH_tenHang);
+    // taoTR_thead.appendChild(taoTH_SL);
+    // taoTR_thead.appendChild(taoTH_donGia);
+    // taoTR_thead.appendChild(taoTH_thanhTien);
 
-    document.getElementById("thead").appendChild(taoTR_thead);
+    // document.getElementById("thead").appendChild(taoTR_thead);
     //end tạo thead
 
     //tạo tbody
@@ -50,7 +51,7 @@ document.getElementById("btnThanhTien").onclick=function(event){
     var taoTD_SL=document.createElement("td");
     taoTD_SL.innerHTML=soLuong;
     var taoTD_donGia=document.createElement("td");
-    taoTD_donGia.innerHTML=dongGiaChuyenDoi+"&nbsp; VND";
+    taoTD_donGia.innerHTML=dongGiaChuyenDoi;
     var taoTD_thanhTien=document.createElement("td");
     taoTD_thanhTien.innerHTML=tienChuyenDoi+"&nbsp; VND";
 
